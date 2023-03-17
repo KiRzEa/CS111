45979202e5e0159876edff547d13345fbdc7fd5b
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2023-03-18 00:34:32 +0700

    Add files via upload

--- CS111.cpp
+++ CS111.cpp
@@ -6,6 +6,26 @@
 
 using namespace std;
 
+set<string> process_text(string str)
+{
+    string word = "";
+    set<string> word_list;
+
+    for (auto x: str)
+    {
+        if ((x >= 48 && x <= 57) || (x >= 65 && x <= 90) || (x >= 97 && x <= 122))
+        {
+            word += x;
+        }
+        else
+        {
+            word_list.insert(word);
+            word = "";
+        }
+    }
+    return word_list;
+}
+
 int main()
 {
     ifstream fileA;
@@ -14,12 +34,10 @@ int main()
     fileA.open("FileA.txt");
     
     string word;
-    int line_number = 0;
     map<string, int> vocab;
 
     while (!fileA.eof())
     {
-        line_number ++;
         getline(fileA, word);
         vocab.insert({word, 1});
     }
@@ -28,14 +46,22 @@ int main()
     fileB.open("FileB.txt");
 
     set<string> B_in_A;
-
+    string line;
+    
     while (!fileB.eof())
     {
-        getline(fileB, word);
-        if (vocab[word] == 1)
-            B_in_A.insert(word);
+        getline(fileB, line);
+        set<string> word_list = process_text(line);
+        for (auto word : word_list)
+        {
+            if (vocab[word] == 1)
+                B_in_A.insert(word);
+        }
     }
     fileB.close();
+
+    cout << B_in_A.size() << endl;
     cout << (double(B_in_A.size()) / double(vocab.size())) * 100 << "%";
+    
     return 0;
 }
